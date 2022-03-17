41bdba1d54528839aafc2580474e41ed4c47f55a
sanjana96m <88242561+sanjana96m@users.noreply.github.com>
2022-03-17 23:21:38 +0530

    first files

---
+++ js/index.js
@@ -0,0 +1,28 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form')
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+window.onscroll = () =>{
+	searchForm.classList.remove('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
+}
